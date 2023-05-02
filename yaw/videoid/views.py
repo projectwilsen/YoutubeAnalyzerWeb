c4cc2f3d010a6eaeb2fc395f0b232105c6981ba7
unknown <projectwilsen@gmail.com>
2023-05-02 20:26:23 +0700

    using youtube api for search video id

--- yaw/videoid/views.py
+++ yaw/videoid/views.py
@@ -1,21 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import scrapetube
+from urllib import response
+from googleapiclient.discovery import build
 
 # Create your views here.
 
 def index(request):
     return render(request, 'form.html')
 
-# def findvideoid(request):
-#     channelid = request.GET["channelid"]
-#     return render(request, 'result.html', {"result":channelid})
+videoIds = []
+
+def get_video_ids(youtube, channelId):
+    """
+    Refer to the documentation: https://googleapis.github.io/google-api-python-client/docs/dyn/youtube_v3.search.html
+    """
+
+    request = youtube.channels().list(
+        part="contentDetails",
+        id=channelId
+    )
+
+    response = request.execute()
+
+    playlistId = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    request = youtube.playlistItems().list(
+        part="contentDetails",
+        playlistId=playlistId,
+        maxResults=50
+    )
+
+    response = request.execute()
+    responseItems = response['items']
+
+    videoIds.extend([item['contentDetails']['videoId'] for item in responseItems])
+
+    # if there is nextPageToken, then keep calling the API
+    while response.get('nextPageToken', None):
+        #print(f'Fetching next page of videos for {channelId}_{playlistId}')
+        request = youtube.playlistItems().list(
+            part="contentDetails",
+            playlistId=playlistId,
+            maxResults=50,
+            pageToken=response['nextPageToken']
+        )
+        response = request.execute()
+        responseItems = response['items']
+
+        videoIds.extend([item['contentDetails']['videoId'] for item in responseItems])
+    
+    #print(f"Finished fetching videoIds for {channelId}. {len(videoIds)} videos found.")
+
+    return videoIds
 
 def findvideoid(request):
     channelid = request.GET["channelid"]
-    videoid = []
-    videos = scrapetube.get_channel(channelid)
-    for video in videos:
-        videoid.append(video['videoId'])
-    return render(request, 'result.html', {"result":videoid})
+    youtube = build("youtube", "v3", developerKey=request.GET["youtubeapikey"])
+    get_video_ids(youtube, channelid)
+    return render(request, 'result.html', {"result":videoIds})
