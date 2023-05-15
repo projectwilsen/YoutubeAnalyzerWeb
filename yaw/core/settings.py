156893235ff614acfe4660e22eaf036022b8cb58
unknown <projectwilsen@gmail.com>
2023-05-16 05:02:13 +0700

    add

--- yaw/core/settings.py
+++ yaw/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#7rw^(21zv87!fhcvq3sa&1xtpknhdru3=+9fd7ba#)4tx)-7j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
