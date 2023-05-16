b12ca035fbc9b65857d8143eb0f62f1882b476de
unknown <projectwilsen@gmail.com>
2023-05-16 11:40:09 +0700

    add

--- yaw/core/settings.py
+++ yaw/core/settings.py
@@ -136,3 +136,5 @@ MEDIA_URL = '/commentsFolder/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://youtube-analyzer.up.railway.app']
