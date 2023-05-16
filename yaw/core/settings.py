8828a533420ab4042cc21a6d40cd50e188913656
unknown <projectwilsen@gmail.com>
2023-05-16 11:23:43 +0700

    change debug setting

--- yaw/core/settings.py
+++ yaw/core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#7rw^(21zv87!fhcvq3sa&1xtpknhdru3=+9fd7ba#)4tx)-7j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*'] 
 
